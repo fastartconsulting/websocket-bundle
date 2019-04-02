af4d93fbe63258d35d08ba659c1c022d091319d8
lmangiacotti <luca.mangiacotti@fastartconsulting.com>
2019-04-02 09:17:43 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,49 @@
+FastART Consulting 
+=============
+
+WebSocketBundle v1.0 STABLE
+=============
+
+This bundle provides a configuration for WebSocket
+
+Integrated with:
+- mongodb
+- voryx/thruway-bundle
+
+
+
+Documentation
+-------------
+- Setup
+- Configuration
+
+Setup
+------------
+- **A) Download the Bundle**
+
+Open a command console, enter your project directory and execute the following command to download the latest stable version of this package:
+
+**`composer require fastartconsulting/websocket-bundle`**
+
+- **B) Add Bundles in AppKernel.php**
+
+Open the file AppKernel.php located inside /app folder and add the following lines:
+
+```
+new Voryx\ThruwayBundle\VoryxThruwayBundle($this)
+```
+
+Configuration
+------------
+
+- Run Server and Enjoy ;) 
+
+
+Our integrations
+------------
+
+License
+-------
+
+This bundle is under the MIT license.
+# websocket-bundle
---
+++ WSListener/MyAuthorizationManager.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace WebSocketBundle\WSListener;
+
+use Thruway\Event\MessageEvent;
+use Thruway\Event\NewRealmEvent;
+use Thruway\Module\RealmModuleInterface;
+use Thruway\Module\RouterModuleClient;
+
+class MyAuthorizationManager extends RouterModuleClient implements RealmModuleInterface {
+
+    /**
+     * @var string
+     */
+    private $realm;
+
+    /**
+     * Constructor
+     *
+     */
+    public function __construct()
+    {
+        $realm = "realm1";
+        $this->realm = $realm;
+        parent::__construct($realm);
+    }
+
+    /**
+     * Listen for Router events.
+     * Required to add the authorization module to the realm
+     *
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'new_realm' => ['handleNewRealm', 10]
+        ];
+    }
+
+    /**
+     * @param NewRealmEvent $newRealmEvent
+     */
+    public function handleNewRealm(NewRealmEvent $newRealmEvent)
+    {
+        $realm = $newRealmEvent->realm;
+
+        if ($realm->getRealmName() === $this->getRealm()) {
+            $realm->addModule($this);
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function getSubscribedRealmEvents()
+    {
+        return [
+            'PublishMessageEvent'   => ['authorize', 100],
+            'SubscribeMessageEvent' => ['authorize', 100],
+            'RegisterMessageEvent'  => ['authorize', 100],
+            'CallMessageEvent'      => ['authorize', 100],
+        ];
+    }
+
+    /**
+     * @param MessageEvent $msg
+     * @return bool
+     */
+    public function authorize(MessageEvent $msg)
+    {
+        //var_dump($msg->session->getAuthenticationDetails()->getAuthId());
+        if ($msg->session->getAuthenticationDetails()->getAuthId() === 'username') {
+            return true;
+        }
+        return false;
+    }
+}
